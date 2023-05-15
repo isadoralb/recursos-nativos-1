7df1fef44618e2e37bc667e84d42cc5d85caf90a
Amanda <eunumseioqpordeemail@gmail.com>
2023-05-15 14:38:35 -0300

    weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- screens/BatteryInfo.jsx
+++ screens/BatteryInfo.jsx
@@ -2,22 +2,24 @@ import { View, Text, StyleSheet } from "react-native";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import * as Battery from 'expo-battery';
+import { useEffect, useState } from "react";
 
 export default function BatteryInfo() {
     const [nivelBateria, setNivelBateria] = useState();
     
     async function Bateria() {
-        setNivelBateria(Battery.getBatteryLevelAsync());
+        const nivel = await Battery.getBatteryLevelAsync();
+        setNivelBateria(nivel * 100);
     }
 
     useEffect(() => {
         Bateria();
-    })
+    },[])
 
     return(
         <View>
-            <Header title="Bateria" />
-            <Text> { nivelBateria } </Text>
+            <Header title="Nível da bateria" />
+            <Text> { nivelBateria } % </Text>
             <Footer />
         </View>
     )
