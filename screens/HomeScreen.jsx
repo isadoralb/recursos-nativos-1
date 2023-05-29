1d75ca377a671bc73a7c37553970ffdb5169a415
Amanda <eunumseioqpordeemail@gmail.com>
2023-05-29 14:40:59 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -3,6 +3,7 @@ import BatteryInfo from "./BatteryInfo";
 import DeviceInfoScreen from "./Deviceinfo";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import ButtonScreen from "./buttonScreen";
+import ScreenOrientationScreen from "./ScreenOrientation";
 
 
 const Stack = createMaterialBottomTabNavigator();
@@ -11,6 +12,7 @@ export default function RootNavigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="ScreenOrientationScreen" component={ScreenOrientationScreen} />
         <Stack.Screen name="BatteryInfo" component={BatteryInfo} />
         <Stack.Screen name="DeviceInfo" component={DeviceInfoScreen} />
         <Stack.Screen name="ButtonScreen" component={ButtonScreen} />
---
+++ screens/ScreenOrientation.jsx
@@ -0,0 +1,90 @@
+import { View, Text, StyleSheet } from "react-native";
+import * as Device from "expo-device";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import * as ScreenOrientation from 'expo-screen-orientation';       
+import { Button } from "react-native";
+
+async function Default(){
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT);
+}
+
+async function DeitarDireita(){
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT);
+}
+
+async function DeitarEsquerda(){
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
+}
+
+async function ForcarNormal(){
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+}
+
+async function ForcarInverter(){
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_DOWN);
+}
+
+export default function ScreenOrientationScreen() {
+  return (
+        <View style={styles.container}>
+          <Header style={styles.title} title="Orientação de Tela" />
+    
+          
+          
+          <Button style={styles.textBtn} title="Padrão" onPress={ Default } />
+          <Button style={styles.textBtn} title="Forçar Deitar Direita" onPress={ DeitarDireita } />
+          <Button style={styles.textBtn} title="Forçar Deitar Esquerda" onPress={ DeitarEsquerda } />
+          <Button style={styles.textBtn} title="Forçar Normal" onPress={ ForcarNormal } />
+          <Button style={styles.textBtn} title="Forçar Inverter" onPress={ ForcarInverter } />
+          <Button style={styles.textBtn} title="Forçar Normal2" onPress={ ForcarNormal } />
+          <Button style={styles.textBtn} title="Informar" onPress={ Default } />
+        </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 15,
+  },
+
+  title: {
+    color: "#fff",
+    backgroundColor: "#606",
+    padding: 10,
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+  textBox: {
+    gap: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  purpleText: {
+    color: "#fff",
+    backgroundColor: "#969",
+    // textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
+    width: 230,
+    height: 60,
+    padding: 10,
+  },
+
+  boxBtn: {
+    backgroundColor: "#7C7B7E",
+    alignItems: 'center',
+    paddingVertical: 10,
+    marginTop: 20,
+  },
+
+  textBtn: {
+    backgroundColor: '#4C9BCC',
+    paddingVertical: 5,
+    paddingHorizontal: 90,
+    color: '#fff'
+  }
+});
